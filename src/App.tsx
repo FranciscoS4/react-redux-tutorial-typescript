5cb74ffdb48f9afb2aa5403a719fc8b0596be326
Francisco Silva Santos <fransssantos2011@gmail.com>
2021-09-02 09:07:42 -0300

    adicionado o provider ao app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 
 import Sidebar from './components/Sidebar';
 import Video from './components/Video';
+import { Provider } from 'react-redux';
+
+import store from './store';
 
 function App() {
   return (
-    <>
+    < Provider store={store}>
       <Video />
       <Sidebar />
-    </>
+    </Provider>
   );
 }
 
